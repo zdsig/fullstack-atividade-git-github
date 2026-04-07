bbab684df3edbde0860a3b8382b7947ecd8743b3
MIKAEL ADRIANO DA COSTA GOMES <a96197522@rn.senac.local>
2026-04-07 14:43:40 -0300

    feat: Adicionando função para calculo da area do trapezio

---
+++ gomes/areaTrapezio.js
@@ -0,0 +1,9 @@
+let b1 = 10;
+let b2 = 6;
+let h = 5;
+
+function calcularAreaTrapezio(b1, b2, h) {
+    return ((b1 + b2) * h) / 2;
+}
+
+console.log( "Área do trapézio: " , calcularAreaTrapezio(b1, b2, h));
